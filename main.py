47f88d3ea784bec5d1bad41a8cf473c12a846d33
JAAR <joe.aaron.acosta@outlook.com>
2023-09-15 18:36:01 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 09/15/2023
+>>> Practicing Fundamentals Program 24
+>>> Version 0
+'''
+
+'''
+>>> Generates a program that opens a file, otherwise, it asks the user to enter a new file if that file doesn't exist. If the file does exist, the user is allowed to write to the file. Finally, the user is given the opportunity to enter a new file name.
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
